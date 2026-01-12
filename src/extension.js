1d994245450ab9f3577e9d6e750d909b07db9bf4
Azir-11 <2075125282@qq.com>
2026-01-12 16:53:08 +0800

    feat: add buddha theme.

---
+++ src/extension.js
@@ -0,0 +1,12 @@
+/**
+ * 激活扩展
+ * @param {object} context - VSCode ExtensionContext
+ */
+async function activate(context) {
+  // 扩展激活成功
+  console.log('Azir Theme extension is now active!')
+}
+
+module.exports = {
+  activate,
+}
